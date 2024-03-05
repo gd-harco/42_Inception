13e09468457b2d7f5866bf3bd6bdedb1b85884de
gd-harco <gd-harco@42Lyon.fr>
2024-03-05 21:20:41 +0100

    added second user creation

--- srcs/requirements/wordpress/script/wpconfig.sh
+++ srcs/requirements/wordpress/script/wpconfig.sh
@@ -20,6 +20,10 @@ then
 					--admin_password="$WP_ADMIN_PASS" \
 					--admin_email="$WP_ADMIN_MAIL"
 
+	wp user create "$WP_BASE_USER" "$WP_BASE_EMAIL" --allow-root \
+													--user_pass="$WP_BASE_PASSWORD"
+
+
 else
 	>&2 echo Wordpress already installed
 fi
